7f5a99523eabda4a5d1a845a77502080e22da0ef
aren talb <aren.talb00@gmail.com>
2024-09-03 20:31:46 +0300

    Create a seeder function to seed my tables with fake data

---
+++ src/lib/seed.js
@@ -0,0 +1,83 @@
+const { faker } = require("@faker-js/faker");
+const { PrismaClient } = require("@prisma/client");
+require("dotenv").config({ path: ".env.local" });
+
+const db = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
+async function main() {
+  console.log("main fun ");
+  const categories = await Promise.all(
+    Array.from({ length: 5 }, () =>
+      db.category.create({
+        data: {
+          name: faker.commerce.department(),
+        },
+      }),
+    ),
+  );
+
+  const users = await Promise.all(
+    Array.from({ length: 10 }, () =>
+      db.user.create({
+        data: {
+          username: faker.internet.userName(),
+          email: faker.internet.email(),
+          password: faker.internet.password(),
+          role: faker.helpers.arrayElement(["user", "admin"]),
+        },
+      }),
+    ),
+  );
+
+  const qoutes = await Promise.all(
+    Array.from({ length: 10 }, () =>
+      db.qoute.create({
+        data: {
+          title: faker.lorem.sentence(),
+          body: faker.lorem.paragraph(),
+          author: faker.name.fullName(),
+          userId: faker.helpers.arrayElement(users).id,
+        },
+      }),
+    ),
+  );
+
+  await Promise.all(
+    Array.from({ length: 20 }, () =>
+      db.comment.create({
+        data: {
+          message: faker.lorem.sentence(),
+          userId: faker.helpers.arrayElement(users).id,
+          qouteId: faker.helpers.arrayElement(qoutes).id,
+        },
+      }),
+    ),
+  );
+
+  await Promise.all(
+    qoutes.map((qoute) =>
+      db.qoute_Category.createMany({
+        data: categories.map((category) => ({
+          qouteId: qoute.id,
+          categoryId: category.id,
+        })),
+      }),
+    ),
+  );
+
+  console.log("Data seeding completed");
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await db.$disconnect();
+  });
