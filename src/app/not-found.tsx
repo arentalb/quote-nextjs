503d0995632181fdbf24fd940a7556525c51953b
aren talb <aren.talb00@gmail.com>
2024-09-12 23:43:21 +0300

    Add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col justify-center items-center mt-20">
+      <div className="mb-8 text-center">
+        <h1 className="text-5xl  capitalize mb-3">Not Found </h1>
+        <p className={"mb-8 text-sm"}>
+          Sorry that we could not find what you were looking for.
+        </p>
+        <Button asChild>
+          <Link href="/">Go to Home</Link>
+        </Button>
+      </div>
+    </div>
+  );
+}
