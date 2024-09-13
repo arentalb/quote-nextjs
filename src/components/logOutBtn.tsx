45e6356ce8efbf571f2ced323adb91a408f6a836
aren talb <aren.talb00@gmail.com>
2024-09-13 19:15:33 +0300

    Fix importing logout function instead of signout function

--- src/components/logOutBtn.tsx
+++ src/components/logOutBtn.tsx
@@ -1,12 +1,12 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { logoutUser } from "@/actions/auth.action";
+import {Button} from "@/components/ui/button";
+import {signOut} from "@/actions/auth.action";
 
 export default function LogOutBtn() {
   return (
     <Button
       onClick={async () => {
-        await logoutUser();
+        await signOut();
       }}
     >
       logout
